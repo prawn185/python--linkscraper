1147812dc15d2b50667c127104a9e325eb0be586
prawn185 <shaunmoore185@gmail.com>
2020-11-16 15:46:54 +0000

    Adding percentage calc

--- main.py
+++ main.py
@@ -12,19 +12,21 @@ import re
 array = ['LINK GOES IN HERE']
 
 def getLinks(array):
+    i = 0
     for url in array:
+        i += 1
+        print(str(round(i / len(array) * 100, 2)) + "%")
         sslinks = []
-        page = urlopen(url)
 
-        soup = BeautifulSoup(page, "html.parser")
+        soup = BeautifulSoup(urlopen(url), "html.parser")
         for link in soup.findAll('a'):
-        //find what?
-            if "find what" in link.get('href'):
+            if "https://ss." in link.get('href'):
                 sslinks.append(link.get('href'))
-                //prints url found on & the link it found
         if len(sslinks) > 0:
+
             print(url)
             print(sslinks)
+
 
 
 getLinks(array)
